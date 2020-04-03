4c74ab87756106e5f5e43d5f5b27fa5d8575a906
Bill Baumgartner <bill.baumgartner@gmail.com>
2020-04-03 10:15:16 -0600

    DRY - consolidation of docID2Content code into PipelineMain
    Consolidated redundant code in the oger and dependency parse pipelines related to retrieving the docId2Content PCollection into PipelineMain.

--- src/main/java/edu/cuanschutz/ccp/tm_provider/etl/OgerPipeline.java
+++ src/main/java/edu/cuanschutz/ccp/tm_provider/etl/OgerPipeline.java
@@ -10,12 +10,9 @@ import java.util.logging.Logger;
 
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.coders.KvCoder;
-import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.gcp.datastore.DatastoreIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.Keys;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
@@ -25,7 +22,6 @@ import org.apache.beam.sdk.values.PCollectionTuple;
 import edu.cuanschutz.ccp.tm_provider.etl.fn.DocumentToEntityFn;
 import edu.cuanschutz.ccp.tm_provider.etl.fn.EtlFailureToEntityFn;
 import edu.cuanschutz.ccp.tm_provider.etl.fn.OgerFn;
-import edu.cuanschutz.ccp.tm_provider.etl.util.DatastoreDocumentUtil;
 import edu.cuanschutz.ccp.tm_provider.etl.util.DatastoreProcessingStatusUtil;
 import edu.cuanschutz.ccp.tm_provider.etl.util.DocumentFormat;
 import edu.cuanschutz.ccp.tm_provider.etl.util.DocumentType;
@@ -46,64 +42,56 @@ public class OgerPipeline {
 
 	public interface Options extends DataflowPipelineOptions {
 		@Description("URI OGER services; pipe-delimited list")
-		String getOgerServiceUris();
+		String getOgerServiceUri();
 
-		void setOgerServiceUris(String value);
+		void setOgerServiceUri(String value);
 
 		@Description("The targetProcessingStatusFlags should align with the concept type served by the OGER service URI; pipe-delimited list")
-		String getTargetProcessStatusFlags();
+		String getTargetProcessStatusFlag();
 
-		void setTargetProcessStatusFlags(String flag);
+		void setTargetProcessStatusFlag(String flag);
 
 		@Description("The targetDocumentTypes should also align with the concept type served by the OGER service URI; pipe-delimited list")
-		String getTargetDocumentTypes();
+		String getTargetDocumentType();
 
-		void setTargetDocumentTypes(String type);
+		void setTargetDocumentType(String type);
 	}
 
 	public static void main(String[] args) {
 		String pipelineVersion = Version.getProjectVersion();
 		com.google.cloud.Timestamp timestamp = com.google.cloud.Timestamp.now();
 		Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
-		LOGGER.log(Level.INFO, String.format("Running OGER pipeline for concepts: ", options.getTargetDocumentTypes()));
+		LOGGER.log(Level.INFO, String.format("Running OGER pipeline for concept: ", options.getTargetDocumentType()));
 
-		List<URI> ogerServiceUris = extractUris(options.getOgerServiceUris());
-		List<ProcessingStatusFlag> processingStatusFlags = extractProcessingStatusFlags(
-				options.getTargetProcessStatusFlags());
-		List<DocumentType> targetDocumentTypes = extractDocumentTypes(options.getTargetDocumentTypes());
-
-		validate(ogerServiceUris, processingStatusFlags, targetDocumentTypes);
+		URI ogerServiceUri = URI.create(options.getOgerServiceUri());
+		ProcessingStatusFlag targetProcessingStatusFlag = ProcessingStatusFlag
+				.valueOf(options.getTargetProcessStatusFlag());
+		DocumentType targetDocumentType = DocumentType.valueOf(options.getTargetDocumentType());
 
 		Pipeline p = Pipeline.create(options);
-		DatastoreProcessingStatusUtil statusUtil = new DatastoreProcessingStatusUtil();
-		// require that the documents have a plain text version to be processed by OGER
-		Set<ProcessingStatusFlag> requiredProcessStatusFlags = EnumSet.of(ProcessingStatusFlag.TEXT_DONE);
 
-		List<PCollection<KV<String, String>>> statusList = new ArrayList<PCollection<KV<String, String>>>();
+		// require that the documents have a plain text version to be processed by OGER
+		Set<ProcessingStatusFlag> requiredProcessStatusFlags = EnumSet.of(ProcessingStatusFlag.TEXT_DONE,
+				ProcessingStatusFlag.DP_DONE);
+
 		/*
 		 * The pipeline will process each triple (URI, ProcessingStatusFlag,
 		 * DocumentType).
 		 */
-		for (int i = 0; i < processingStatusFlags.size(); i++) {
-			ProcessingStatusFlag targetProcessStatusFlag = processingStatusFlags.get(i);
-			DocumentType documentType = targetDocumentTypes.get(i);
-			URI ogerServiceUri = ogerServiceUris.get(i);
-			LOGGER.log(Level.INFO, String.format("Initializing pipeline for: %s -- %s -- %s",
-					targetProcessStatusFlag.name(), documentType.name(), ogerServiceUri));
+		LOGGER.log(Level.INFO, String.format("Initializing pipeline for: %s -- %s -- %s",
+				targetProcessingStatusFlag.name(), targetDocumentType.name(), ogerServiceUri));
 
-			PCollection<KV<String, String>> docId2Content = getDocumentsToProcess(pipelineVersion, p, statusUtil,
-					requiredProcessStatusFlags, targetProcessStatusFlag, documentType);
+		PCollection<KV<String, String>> docId2Content = PipelineMain.getDocId2Content(pipelineVersion,
+				options.getProject(), p, targetProcessingStatusFlag, requiredProcessStatusFlags);
 
-			PCollectionTuple output = OgerFn.process(docId2Content, ogerServiceUri.toString(), PIPELINE_KEY,
-					pipelineVersion, documentType, timestamp);
+		PCollectionTuple output = OgerFn.process(docId2Content, ogerServiceUri.toString(), PIPELINE_KEY,
+				pipelineVersion, targetDocumentType, timestamp);
 
-			PCollection<KV<String, String>> docIdToStatusToUpdate = logResults(
-					docId2Content.apply(Keys.<String>create()), pipelineVersion, options.getProject(),
-					targetProcessStatusFlag, documentType, output);
+		PCollection<KV<String, String>> docIdToStatusToUpdate = logResults(docId2Content.apply(Keys.<String>create()),
+				pipelineVersion, options.getProject(), targetProcessingStatusFlag, targetDocumentType, output);
 
-			statusList.add(docIdToStatusToUpdate);
-		}
-
+		List<PCollection<KV<String, String>>> statusList = new ArrayList<PCollection<KV<String, String>>>();
+		statusList.add(docIdToStatusToUpdate);
 		// update the status for documents that were successfully processed. To the
 		// update in batch, e.g one update per document.
 		DatastoreProcessingStatusUtil.performStatusUpdatesInBatch(statusList);
@@ -150,107 +138,6 @@ public class OgerPipeline {
 				DatastoreIO.v1().write().withProjectId(projectId));
 
 		return DatastoreProcessingStatusUtil.getSuccessStatus(processedDocIds, failures, targetProcessStatusFlag);
-	}
-
-	/**
-	 * Queries Cloud Datastore to find documents that have the
-	 * targetProcessingStatusFlag = false, and that also have any required
-	 * prerequisite processing already complete.
-	 * 
-	 * @param pipelineVersion
-	 * @param p
-	 * @param statusUtil
-	 * @param requiredProcessStatusFlags
-	 * @param targetProcessStatusFlag
-	 * @param documentType
-	 * @return
-	 */
-	private static PCollection<KV<String, String>> getDocumentsToProcess(String pipelineVersion, Pipeline p,
-			DatastoreProcessingStatusUtil statusUtil, Set<ProcessingStatusFlag> requiredProcessStatusFlags,
-			ProcessingStatusFlag targetProcessStatusFlag, DocumentType documentType) {
-		// query Cloud Datastore to find document IDs in need of processing
-		List<String> documentIdsToProcess = statusUtil.getDocumentIdsInNeedOfProcessing(targetProcessStatusFlag,
-				requiredProcessStatusFlags);
-
-		LOGGER.log(Level.INFO, String.format("Pipeline: %s-%s, %d documents to process...", PIPELINE_KEY.name(),
-				documentType.name(), documentIdsToProcess.size()));
-
-		// query for the document content for those document IDs - Note that the plain
-		// text files are generated by the bioc-to-text pipeline
-		List<KV<String, String>> documentIdToContentList = new DatastoreDocumentUtil().getDocumentIdToContentList(
-				documentIdsToProcess, DocumentType.TEXT, DocumentFormat.TEXT, PipelineKey.BIOC_TO_TEXT,
-				pipelineVersion);
-
-		LOGGER.log(Level.INFO, String.format("Pipeline: %s-%s, %d documents retrieved...", PIPELINE_KEY.name(),
-				documentType.name(), documentIdToContentList.size()));
-
-		// create initial PCollection
-		PCollection<KV<String, String>> docId2Content = p.apply(
-				Create.of(documentIdToContentList).withCoder(KvCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of())));
-		return docId2Content;
-	}
-
-	/**
-	 * ensure an equal number of values in each parameter list
-	 * 
-	 * @param ogerServiceUris
-	 * @param processingStatusFlags
-	 * @param targetDocumentTypes
-	 */
-	private static void validate(List<URI> ogerServiceUris, List<ProcessingStatusFlag> processingStatusFlags,
-			List<DocumentType> targetDocumentTypes) {
-		int size1 = ogerServiceUris.size();
-		int size2 = processingStatusFlags.size();
-		int size3 = targetDocumentTypes.size();
-
-		if (!(size1 == size2 && size1 == size3)) {
-			throw new IllegalArgumentException(
-					String.format("Detected mismatch in number of input arguments. Please adjust and re-try. "
-							+ "service-uris=%d; status-flags=%d; document-types=%d", size1, size2, size3));
-		}
-
-	}
-
-	/**
-	 * @param targetDocumentTypes
-	 * @return a list of {@link DocumentType} given the input pipe-delimited list of
-	 *         document type names
-	 */
-	private static List<DocumentType> extractDocumentTypes(String targetDocumentTypes) {
-		List<DocumentType> types = new ArrayList<DocumentType>();
-		String[] toks = targetDocumentTypes.split("\\|");
-		for (String tok : toks) {
-			types.add(DocumentType.valueOf(tok.toUpperCase()));
-		}
-		return types;
-	}
-
-	/**
-	 * @param targetProcessStatusFlags
-	 * @return a list of {@link ProcessingStatusFlag} given the input pipe-delimited
-	 *         list of processing status flag names
-	 */
-	private static List<ProcessingStatusFlag> extractProcessingStatusFlags(String targetProcessStatusFlags) {
-		List<ProcessingStatusFlag> flags = new ArrayList<ProcessingStatusFlag>();
-		String[] toks = targetProcessStatusFlags.split("\\|");
-		for (String tok : toks) {
-			flags.add(ProcessingStatusFlag.valueOf(tok.toUpperCase()));
-		}
-		return flags;
-	}
-
-	/**
-	 * @param ogerServiceUris
-	 * @return a list of {@link URI} given the input pipe-delimited list of URI
-	 *         strings
-	 */
-	private static List<URI> extractUris(String ogerServiceUris) {
-		List<URI> flags = new ArrayList<URI>();
-		String[] toks = ogerServiceUris.split("\\|");
-		for (String tok : toks) {
-			flags.add(URI.create(tok));
-		}
-		return flags;
 	}
 
 }
--- src/main/java/edu/cuanschutz/ccp/tm_provider/etl/PipelineMain.java
+++ src/main/java/edu/cuanschutz/ccp/tm_provider/etl/PipelineMain.java
@@ -1,9 +1,36 @@
 package edu.cuanschutz.ccp.tm_provider.etl;
 
+import static com.google.datastore.v1.client.DatastoreHelper.makeFilter;
+import static com.google.datastore.v1.client.DatastoreHelper.makeValue;
+import static edu.cuanschutz.ccp.tm_provider.etl.util.DatastoreConstants.STATUS_KIND;
+import static edu.cuanschutz.ccp.tm_provider.etl.util.DatastoreConstants.STATUS_PROPERTY_DOCUMENT_ID;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.datastore.DatastoreIO;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+
+import com.google.datastore.v1.CompositeFilter;
+import com.google.datastore.v1.CompositeFilter.Operator;
+import com.google.datastore.v1.Entity;
+import com.google.datastore.v1.Filter;
+import com.google.datastore.v1.PropertyFilter;
+import com.google.datastore.v1.Query;
+import com.google.datastore.v1.Value;
+
+import edu.cuanschutz.ccp.tm_provider.etl.util.DatastoreDocumentUtil;
+import edu.cuanschutz.ccp.tm_provider.etl.util.DocumentFormat;
+import edu.cuanschutz.ccp.tm_provider.etl.util.DocumentType;
 import edu.cuanschutz.ccp.tm_provider.etl.util.PipelineKey;
+import edu.cuanschutz.ccp.tm_provider.etl.util.ProcessingStatusFlag;
 import edu.cuanschutz.ccp.tm_provider.etl.util.Version;
 
 public class PipelineMain {
@@ -37,4 +64,58 @@ public class PipelineMain {
 			}
 		}
 	}
+
+	public static PCollection<KV<String, String>> getDocId2Content(String pipelineVersion, String project, Pipeline p,
+			ProcessingStatusFlag targetProcessStatusFlag, Set<ProcessingStatusFlag> requiredProcessStatusFlags) {
+		PCollection<Entity> status = getStatusEntitiesToProcess(p, targetProcessStatusFlag, requiredProcessStatusFlags,
+				project);
+
+		PCollection<KV<String, String>> docId2Content = status.apply("get document content",
+				ParDo.of(new DoFn<Entity, KV<String, String>>() {
+					private static final long serialVersionUID = 1L;
+
+					@ProcessElement
+					public void processElement(@Element Entity status, OutputReceiver<KV<String, String>> out) {
+						Value value = status.getPropertiesMap().get(STATUS_PROPERTY_DOCUMENT_ID);
+						String documentId = value.getStringValue();
+
+						DatastoreDocumentUtil util = new DatastoreDocumentUtil();
+						KV<String, String> documentIdToContent = util.getDocumentIdToContent(documentId,
+								DocumentType.TEXT, DocumentFormat.TEXT, PipelineKey.BIOC_TO_TEXT, pipelineVersion);
+						if (documentIdToContent != null) {
+							out.output(documentIdToContent);
+						}
+
+					}
+
+				}));
+		return docId2Content;
+	}
+
+	public static PCollection<Entity> getStatusEntitiesToProcess(Pipeline p,
+			ProcessingStatusFlag targetProcessStatusFlag, Set<ProcessingStatusFlag> requiredProcessStatusFlags,
+			String project) {
+		List<Filter> filters = new ArrayList<Filter>();
+		for (ProcessingStatusFlag flag : requiredProcessStatusFlags) {
+			Filter filter = makeFilter(flag.getDatastorePropertyName(), PropertyFilter.Operator.EQUAL, makeValue(true))
+					.build();
+			filters.add(filter);
+		}
+		filters.add(makeFilter(targetProcessStatusFlag.getDatastorePropertyName(), PropertyFilter.Operator.EQUAL,
+				makeValue(false)).build());
+
+		Query.Builder query = Query.newBuilder();
+		query.addKindBuilder().setName(STATUS_KIND);
+
+		CompositeFilter.Builder compositeFilter = CompositeFilter.newBuilder();
+		compositeFilter.addAllFilters(filters);
+		compositeFilter.setOp(Operator.AND);
+		Filter filter = Filter.newBuilder().setCompositeFilter(compositeFilter).build();
+		query.setFilter(filter);
+
+		PCollection<Entity> status = p.apply("document_entity->datastore",
+				DatastoreIO.v1().read().withQuery(query.build()).withProjectId(project));
+		return status;
+	}
+
 }
