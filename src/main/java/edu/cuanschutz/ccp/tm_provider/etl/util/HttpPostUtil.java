9a9a3471a655b46fc6366b78795bbd1c45e347c8
Bill Baumgartner <bill.baumgartner@gmail.com>
2020-09-03 14:40:21 -0600

    Update HttpPostUtil.java
    Added timeouts so that processes don't hang while waiting for an HTTP response.

--- src/main/java/edu/cuanschutz/ccp/tm_provider/etl/util/HttpPostUtil.java
+++ src/main/java/edu/cuanschutz/ccp/tm_provider/etl/util/HttpPostUtil.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.EntityBuilder;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
@@ -22,17 +23,24 @@ public class HttpPostUtil {
 	private final String targetUri;
 
 	public String submit(String payload) throws IOException {
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(targetUri);
+		int timeout = 5; // seconds
+		RequestConfig config = RequestConfig.custom().setConnectTimeout(timeout * 1000)
+				.setConnectionRequestTimeout(timeout * 1000).setSocketTimeout(timeout * 1000).build();
+		try (CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build()) {
 
-		EntityBuilder builder = EntityBuilder.create();
-		builder.setContentType(ContentType.TEXT_PLAIN);
-		builder.setBinary(payload.getBytes());
-		HttpEntity entity = builder.build();
+			HttpPost post = new HttpPost(targetUri);
 
-		post.setEntity(entity);
-		HttpResponse response = client.execute(post);
-		return EntityUtils.toString(response.getEntity());
+			EntityBuilder builder = EntityBuilder.create();
+			builder.setContentType(ContentType.TEXT_PLAIN);
+			builder.setBinary(payload.getBytes());
+			HttpEntity entity = builder.build();
+
+			post.setEntity(entity);
+			HttpResponse response = client.execute(post);
+
+			response.getStatusLine().getStatusCode();
+			return EntityUtils.toString(response.getEntity());
+		}
 
 	}
 
