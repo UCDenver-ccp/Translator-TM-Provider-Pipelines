f7a10039a20378800d442b5a6ba424a46742a45c
Bill Baumgartner <bill.baumgartner@gmail.com>
2021-01-22 12:25:10 -0700

    add PIPELINE_KEY as parameter

--- scripts/pipelines/concepts/run_oger.sh
+++ scripts/pipelines/concepts/run_oger.sh
@@ -6,11 +6,12 @@ PROJECT=$3
 COLLECTION=$4
 STAGE_LOCATION=$5
 TMP_LOCATION=$6
+PIPELINE_KEY=$7
 
 
 TPSF="OGER_${ONT}_DONE"
 TDT="CONCEPT_${ONT}"
-JOB_NAME=$(echo "OGER-${ONT}-${COLLECTION}" | tr '_' '-')
+JOB_NAME=$(echo "OGER-${ONT}-${COLLECTION}-${PIPELINE_KEY}" | tr '_' '-')
 
 
 echo "SERVICE URL: $SERVICE_URL"
@@ -20,6 +21,7 @@ echo "PROJECT: $PROJECT"
 echo "TPSF: $TPSF"
 echo "TDT: $TDT"
 echo "JOB_NAME: $JOB_NAME"
+echo "PIPELINE KEY: $PIPELINE_KEY"
 
 java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-0.1.0.jar OGER \
 --jobName="$JOB_NAME" \
@@ -27,11 +29,11 @@ java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-0.1.0.jar OGER \
 --ogerOutputType=TSV \
 --targetProcessingStatusFlag="$TPSF" \
 --targetDocumentType="$TDT" \
---targetDocumentFormat='BIONLP' \
---inputPipelineKey='MEDLINE_XML_TO_TEXT' \
+--targetDocumentFormat="BIONLP" \
+--inputPipelineKey="$PIPELINE_KEY" \
 --inputPipelineVersion='0.1.0' \
 --collection="$COLLECTION" \
---overwrite='YES' \
+--overwrite="YES" \
 --project="${PROJECT}" \
 --stagingLocation="$STAGE_LOCATION" \
 --gcpTempLocation="$TMP_LOCATION" \
--- scripts/pipelines/sentences/run_sentence_segment.sh
+++ scripts/pipelines/sentences/run_sentence_segment.sh
@@ -4,6 +4,7 @@ PROJECT=$1
 COLLECTION=$2
 STAGE_LOCATION=$3
 TMP_LOCATION=$4
+PIPELINE_KEY=$5
 
 
 JOB_NAME=$(echo "SENTENCE-${COLLECTION}" | tr '_' '-')
@@ -15,7 +16,7 @@ echo "JOB_NAME: $JOB_NAME"
 
 java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-0.1.0.jar SENTENCE_SEGMENTATION \
 --jobName="$JOB_NAME" \
---inputPipelineKey='MEDLINE_XML_TO_TEXT' \
+--inputPipelineKey="$PIPELINE_KEY" \
 --inputPipelineVersion='0.1.0' \
 --collection="$COLLECTION" \
 --overwrite='YES' \
