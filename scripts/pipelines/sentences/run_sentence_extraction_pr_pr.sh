8a7c6524c3ca409a908e8fee537837e0fbfd2079
Bill Baumgartner <bill.baumgartner@gmail.com>
2021-03-17 12:59:31 -0600

    Update sentence extraction scripts
    added TEXT_PIPELINE_KEY as an input argument

--- scripts/pipelines/sentences/run_sentence_extraction_pr_pr.sh
+++ scripts/pipelines/sentences/run_sentence_extraction_pr_pr.sh
@@ -5,6 +5,7 @@ COLLECTION=$2
 STAGE_LOCATION=$3
 TMP_LOCATION=$4
 BUCKET=$5
+TEXT_PIPELINE_KEY=$6
 
 
 JOB_NAME=$(echo "SENTENCE-EXTRACTION-PR-PR-${COLLECTION}" | tr '_' '-')
@@ -14,12 +15,12 @@ echo "COLLECTION: $COLLECTION"
 echo "PROJECT: $PROJECT"
 echo "JOB_NAME: $JOB_NAME"
 
-OUTPUT_BUCKET="${BUCKET}/extracted-sentences/pr-pr/pr-pr"
+OUTPUT_BUCKET="${BUCKET}/output/sentences/pr-pr/pr-pr"
 
 java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-0.1.0.jar SENTENCE_EXTRACTION \
 --jobName="$JOB_NAME" \
 --targetProcessingStatusFlag='SENTENCE_DONE' \
---inputDocumentCriteria='TEXT|TEXT|MEDLINE_XML_TO_TEXT|0.1.0;SENTENCE|BIONLP|SENTENCE_SEGMENTATION|0.1.0;CONCEPT_ALL|BIONLP|CONCEPT_POST_PROCESS|0.1.0' \
+--inputDocumentCriteria="TEXT|TEXT|$TEXT_PIPELINE_KEY|0.1.0;SENTENCE|BIONLP|SENTENCE_SEGMENTATION|0.1.0;CONCEPT_ALL|BIONLP|CONCEPT_POST_PROCESS|0.1.0" \
 --keywords='' \
 --collection="$COLLECTION" \
 --overwrite='YES' \
@@ -31,7 +32,8 @@ java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-0.1.0.jar SENTENCE_E
 --project="${PROJECT}" \
 --stagingLocation="$STAGE_LOCATION" \
 --gcpTempLocation="$TMP_LOCATION" \
---zone=us-central1-c \
+--workerZone=us-central1-c \
+--region=us-central1 \
 --numWorkers=10 \
 --maxNumWorkers=125 \
 --runner=DataflowRunner
