e9063f684c7bac6334782d97ff8ee6aa26128167
Bill Baumgartner <bill.baumgartner@gmail.com>
2023-08-02 09:43:13 -0600

    Update ChebiOgerDictFileFactory.java
    added filterSpecificSynonyms()

--- src/main/java/edu/cuanschutz/ccp/tm_provider/oger/dict/ChebiOgerDictFileFactory.java
+++ src/main/java/edu/cuanschutz/ccp/tm_provider/oger/dict/ChebiOgerDictFileFactory.java
@@ -2,8 +2,10 @@ package edu.cuanschutz.ccp.tm_provider.oger.dict;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import edu.cuanschutz.ccp.tm_provider.oger.util.OgerDictFileFactory;
@@ -23,36 +25,39 @@ public class ChebiOgerDictFileFactory extends OgerDictFileFactory {
 		super("chemical", "CHEBI", SynonymSelection.EXACT_ONLY, EXCLUDED_ROOT_CLASSES);
 	}
 
-	public static final Set<String> EXCLUDED_INDIVIDUAL_CLASSES = new HashSet<String>(Arrays.asList(OBO_PURL + "CHEBI_15035", // retinal
-			OBO_PURL + "CHEBI_18367", // phosphate
-			OBO_PURL + "CHEBI_26020", // phosphate
-			OBO_PURL + "CHEBI_36976", // nucleotide
-			OBO_PURL + "CHEBI_16670", // peptide
-			OBO_PURL + "CHEBI_7998", // peptide
-			OBO_PURL + "CHEBI_7999", // peptide
-			OBO_PURL + "CHEBI_8001", // peptide
-			OBO_PURL + "CHEBI_36080", // protein
-			OBO_PURL + "CHEBI_8580", // protein
-			OBO_PURL + "CHEBI_2645", // amino acid
-			OBO_PURL + "CHEBI_33709", // amino acid
-			OBO_PURL + "CHEBI_33731", // Cluster
-			OBO_PURL + "CHEBI_8762", // RNA
-			OBO_PURL + "CHEBI_60004", // mixture
-			OBO_PURL + "CHEBI_5386", // globin
-			OBO_PURL + "CHEBI_33696", // nucleic acid
-			OBO_PURL + "CHEBI_49807", // lead
-			OBO_PURL + "CHEBI_15841", // polypeptides
-			OBO_PURL + "CHEBI_79381", // a-factor
-			OBO_PURL + "CHEBI_18059", // lipid
-			OBO_PURL + "CHEBI_75958", // solution
-			OBO_PURL + "CHEBI_24870" // ion
-	));
+	public static final Set<String> EXCLUDED_INDIVIDUAL_CLASSES = new HashSet<String>(
+			Arrays.asList(OBO_PURL + "CHEBI_15035", // retinal
+					OBO_PURL + "CHEBI_18367", // phosphate
+					OBO_PURL + "CHEBI_26020", // phosphate
+					OBO_PURL + "CHEBI_36976", // nucleotide
+					OBO_PURL + "CHEBI_16670", // peptide
+					OBO_PURL + "CHEBI_7998", // peptide
+					OBO_PURL + "CHEBI_7999", // peptide
+					OBO_PURL + "CHEBI_8001", // peptide
+					OBO_PURL + "CHEBI_36080", // protein
+					OBO_PURL + "CHEBI_8580", // protein
+					OBO_PURL + "CHEBI_2645", // amino acid
+					OBO_PURL + "CHEBI_33709", // amino acid
+					OBO_PURL + "CHEBI_33731", // Cluster
+					OBO_PURL + "CHEBI_8762", // RNA
+					OBO_PURL + "CHEBI_60004", // mixture
+					OBO_PURL + "CHEBI_5386", // globin
+					OBO_PURL + "CHEBI_33696", // nucleic acid
+					OBO_PURL + "CHEBI_49807", // lead
+					OBO_PURL + "CHEBI_15841", // polypeptides
+					OBO_PURL + "CHEBI_79381", // a-factor
+					OBO_PURL + "CHEBI_18059", // lipid
+					OBO_PURL + "CHEBI_75958", // solution
+					OBO_PURL + "CHEBI_27889", // lead
+					OBO_PURL + "CHEBI_24870" // ion
+			));
 
 	@Override
 	protected Set<String> augmentSynonyms(String iri, Set<String> syns) {
 		Set<String> toReturn = removeStopWords(syns);
 		toReturn = removeWordsLessThenLength(toReturn, 3);
 		toReturn = filterSynonyms(toReturn);
+		toReturn = filterSpecificSynonyms(iri, toReturn);
 
 		if (EXCLUDED_INDIVIDUAL_CLASSES.contains(iri)) {
 			toReturn = Collections.emptySet();
@@ -74,6 +79,21 @@ public class ChebiOgerDictFileFactory extends OgerDictFileFactory {
 		}
 
 		return filtered;
+	}
+
+	protected static Set<String> filterSpecificSynonyms(String iri, Set<String> syns) {
+
+		Map<String, Set<String>> map = new HashMap<String, Set<String>>();
+
+		map.put(OBO_PURL + "CHEBI_37409", new HashSet<String>(Arrays.asList("crown")));
+
+		Set<String> updatedSyns = new HashSet<String>(syns);
+
+		if (map.containsKey(iri)) {
+			updatedSyns.removeAll(map.get(iri));
+		}
+
+		return updatedSyns;
 	}
 
 }
