2db1b3ffbfdcb80d6a7b8418b861d1e49a56a3af
Bill Baumgartner <bill.baumgartner@gmail.com>
2023-08-08 08:50:13 -0600

    script updates

--- scripts/pipelines/concepts/run_abbreviations.sh
+++ scripts/pipelines/concepts/run_abbreviations.sh
@@ -3,21 +3,25 @@
 AB3P_SOURCE_PATH=$1
 WORKER_PATH=$2
 TEXT_PIPELINE_KEY=$3
-PROJECT=$4
-COLLECTION=$5
-STAGE_LOCATION=$6
-TMP_LOCATION=$7
+TEXT_PIPELINE_VERSION=$4
+SENTENCE_PIPELINE_VERSION=$5
+PROJECT=$6
+COLLECTION=$7
+STAGE_LOCATION=$8
+TMP_LOCATION=$9
+JAR_VERSION=${10}
 
 JOB_NAME=$(echo "ABBREVIATION-${COLLECTION}" | tr '_' '-')
 
 echo "AB3P_SOURCE_PATH: $AB3P_SOURCE_PATH"
 echo "WORKER_PATH: $WORKER_PATH"
 echo "TEXT_PIPELINE_KEY: $TEXT_PIPELINE_KEY"
+echo "TEXT_PIPELINE_VERSION: $TEXT_PIPELINE_VERSION"
 echo "COLLECTION: $COLLECTION"
 echo "PROJECT: $PROJECT"
 echo "JOB_NAME: $JOB_NAME"
 
-java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-0.1.0.jar ABBREVIATION \
+java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-${JAR_VERSION}.jar ABBREVIATION \
 --jobName="$JOB_NAME" \
 --abbreviationBinaryName="identify_abbr" \
 --binaryFileAndDependencies="identify_abbr|path_Ab3P|WordData/Ab3P_prec.dat|WordData/Lf1chSf|WordData/SingTermFreq.dat|WordData/cshset_wrdset3.ad|WordData/cshset_wrdset3.ct|WordData/cshset_wrdset3.ha|WordData/cshset_wrdset3.nm|WordData/cshset_wrdset3.str|WordData/hshset_Lf1chSf.ad|WordData/hshset_Lf1chSf.ha|WordData/hshset_Lf1chSf.nm|WordData/hshset_Lf1chSf.str|WordData/hshset_stop.ad|WordData/hshset_stop.ha|WordData/hshset_stop.nm|WordData/hshset_stop.str|WordData/stop" \
@@ -25,9 +29,9 @@ java -Dfile.encoding=UTF-8 -jar target/tm-pipelines-bundled-0.1.0.jar ABBREVIATI
 --workerPath="$WORKER_PATH" \
 --concurrency=1 \
 --inputTextPipelineKey="$TEXT_PIPELINE_KEY" \
---inputTextPipelineVersion='0.1.0' \
+--inputTextPipelineVersion="$TEXT_PIPELINE_VERSION" \
 --inputSentencePipelineKey='SENTENCE_SEGMENTATION' \
---inputSentencePipelineVersion='0.1.0' \
+--inputSentencePipelineVersion="${SENTENCE_PIPELINE_VERSION}" \
 --collection="$COLLECTION" \
 --overwrite='YES' \
 --project="${PROJECT}" \
